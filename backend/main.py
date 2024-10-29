c6906d805febab6f362024eca3ecff2a24f0e3a2
graceynguyen <101678455+graceynguyen@users.noreply.github.com>
2024-10-29 11:01:46 -0400

    create api call for student courses (in progress)

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+import sqlite3
+import sqlalchemy
+import pandas as pd
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/StudentCourses", methods=['GET'])
+def StudentCourses():
+    # get current student info
+    current_student = '123'
+
+    conn = sqlite3.connect('eduPortalDB.db')
+
+    query ="select course.courseName as course, course.semester as semester, course.year as year, course.credits as credits, taken.grade as grade " + \
+            "from taken inner join course on taken.course = course.id " + \
+            "where taken.student = '" + current_student + "';"
+    student_course_df = pd.read_sql_query(query)
+
+    student_course_json = student_course_df.to_json(orient='records')
+
+    conn.close()
+
+    return student_course_json
