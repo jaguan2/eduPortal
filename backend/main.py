dad31014a3f48f61ac7f07b4ef7986e1b4f8c5a7
Kelly Ton <kthton@gmail.com>
2024-11-16 15:25:41 -0500

    CORS and Profile component

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,11 @@ import sqlite3
 import sqlalchemy
 import pandas as pd
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/StudentCourses", methods=['GET'])
 def StudentCourses():
