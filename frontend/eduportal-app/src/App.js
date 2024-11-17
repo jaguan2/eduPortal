eeb1fd7739964bedea36265e53aa3bfe5dd091c5
Trudy Kwok <kwokt3@gmail.com>
2024-11-17 10:21:01 -0500

    Commented out StaffDashboard, implement incomplete.

--- frontend/eduportal-app/src/App.js
+++ frontend/eduportal-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import StudentDashboard from './Routes/StudentDashboard';
-import StaffDashboard from './Routes/StaffDashboard'
+// import StaffDashboard from './Routes/StaffDashboard'
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
