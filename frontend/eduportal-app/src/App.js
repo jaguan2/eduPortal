049c4858586a0b632a7d0710810ad5b5a3ff519e
Kelly Ton <kthton@gmail.com>
2024-11-16 23:23:20 -0500

    bootstrap conversion from mui -- fix course table

--- frontend/eduportal-app/src/App.js
+++ frontend/eduportal-app/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import CourseTable from './Components/StudentCourseTable';
 import StudentNavigation from './Components/StudentNavigation';
 import Profile from './Components/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Profile/>
       <StudentNavigation/>
+      <CourseTable/>
     </div>
   );
 }
